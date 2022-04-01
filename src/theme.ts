bdb0e8e3886107d0bd10d919f52d65d48a5eebf1
Joel Cowie <joelcowie@Joels-MacBook-Pro.local>
2022-04-01 08:49:03 -0600

    update theme, header text

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+import { green } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#17689E',
+    },
+    secondary: {
+      main: '#ff6666',
+    },
+  },
+});
+
+export default theme;
